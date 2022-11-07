2a66a3f0d61bc54cd5ddeb356537958e8b912ec2
علی خداوردی <alikhodaveerdi@yahoo.com>
2022-11-07 18:33:00 +0330

    مدل چند به چند

--- React-Asp-Api/Models/Pokemon.cs
+++ React-Asp-Api/Models/Pokemon.cs
@@ -8,5 +8,7 @@
         public DateTime BrithDate { get; set; }
 
         public ICollection<Review> Reviews { get; set; }
+        public ICollection<PokemonOwner> PokemonOwner { get; set; }
+        public ICollection<PokemonCategory> PokemonCategory { get; set; }
     }
 }
